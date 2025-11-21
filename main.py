4eda33653870ccc319702a892e03f63f60782c86
paul-london <palondon@hotmail.com>
2025-11-20 22:53:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from src.agents.fetcher import PubMedFetcher
+import streamlit as st
+import pandas as pd
+import json
 
-# Example RSS feeds (you will replace these with real PubMed RSS URLs)
-rss_feeds = [
-    "https://pubmed.ncbi.nlm.nih.gov/rss/search/1WnQjLbmkKziVwMd.../",  # CRISPR
-    "https://pubmed.ncbi.nlm.nih.gov/rss/search/3k2398dkf.../",         # Gene therapy
-]
+from src.agents.fetcher import PubMedFetcher
 
 fetcher = PubMedFetcher(rss_urls=rss_feeds)
 articles = fetcher.fetch()
